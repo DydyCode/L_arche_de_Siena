3344ede2079fc93cf4c6f783edc70681cc7c56c0
DydyCode <80527449+DydyCode@users.noreply.github.com>
2021-09-28 18:13:22 +0200

    MAJ

---
+++ app.js
@@ -0,0 +1,41 @@
+// const hero = document.querySelector('.hero');
+// const slider = document.querySelector('.slider');
+// const headline = document.querySelector('.headline');
+// const nav = document.querySelector('#nav');
+
+// const tl = new TimelineMax();
+
+// tl.fromTo(
+//     hero,
+//     1,
+//     {height: "0%"},
+//     {height:"80%", ease: Power2.easeInOut}
+// ).fromTo(
+//     hero,
+//     1.2,
+//     {width: "100%"},
+//     {width: "80%"}
+// ).fromTo(
+//     slider,
+//     1.2,
+//     {x: "-100%"},
+//     {x:"0%", ease: Power2.easeInOut},
+//     "-=1.2"
+// ).fromTo(
+//     nav,
+//     0.5,
+//     {opacity: 0, x: 30},
+//     {opacity: 1, x: 0},
+//     "-=1"
+// );
+
+// Anime au scroll //
+let cards = document.getElementById('container4');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 900) {
+        cards.classList.add('anim-cards');
+    }else {
+        cards.classList.remove('anim-cards');
+    }
+})
